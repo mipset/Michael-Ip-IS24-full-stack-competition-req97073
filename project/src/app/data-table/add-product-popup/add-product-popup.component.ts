7378703960e55f2f586a126299b03d6c4780f610
mipset <michaelip90@hotmail.com>
2023-03-28 01:50:49 -0700

    Fixed Expansion. Edit Field Started

--- project/src/app/data-table/add-product-popup/add-product-popup.component.ts
+++ project/src/app/data-table/add-product-popup/add-product-popup.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { DataTableService } from '../data-table.service';
 
 @Component({
   selector: 'app-add-product-popup',
   templateUrl: './add-product-popup.component.html',
-  styleUrls: ['./add-product-popup.component.css']
+  styleUrls: ['./add-product-popup.component.css'],
 })
 export class AddProductPopupComponent {
-  constructor(private dataTableService: DataTableService, private fb: FormBuilder, public dialogRef: MatDialogRef<AddProductPopupComponent>) { }
+  constructor(
+    private dataTableService: DataTableService,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<AddProductPopupComponent>
+  ) {}
 
   newProductForm: FormGroup = new FormGroup({});
 
   ngOnInit() {
     this.initializeForm();
-
   }
 
   initializeForm() {
@@ -25,53 +34,57 @@ export class AddProductPopupComponent {
       scrumMaster: [''],
       startDate: [''],
       methodology: [''],
-      developers: this.fb.array([])
-    })
+      developers: this.fb.array([]),
+    });
     this.addDeveloper();
   }
 
   get developers() {
-    return this.newProductForm.controls["developers"] as FormArray;
+    return this.newProductForm.controls['developers'] as FormArray;
   }
 
-
-  addDeveloper(){
-    if(this.developers.length>4){
-      alert("limit 5 developers");
+  addDeveloper() {
+    if (this.developers.length > 4) {
+      alert('limit 5 developers');
       return;
     }
     const newDeveloper = this.fb.group({
-      developers: ['']
+      developers: [''],
     });
     this.developers.push(newDeveloper);
   }
+
+  removeDeveloper(i: number) {
+    this.developers.removeAt(i);
+  }
   checkValidText(x: any) {
     if (!/^[a-zA-Z ]*$/.test(x.target.value)) {
-      console.log("Letters Only Please");
+      console.log('Letters Only Please');
     }
   }
 
   addProduct() {
     let parseDevelopers: string[] = [];
     if (this.newProductForm.invalid) {
-      alert("Missing Fields");
-    }
-    else {
+      alert('Missing Fields');
+    } else {
       console.log(this.newProductForm);
-      
-      for (let i = 0; i<this.newProductForm.value.developers.length; i++){
-        parseDevelopers.push(this.newProductForm.value.developers[i].developers);
+
+      for (let i = 0; i < this.newProductForm.value.developers.length; i++) {
+        parseDevelopers.push(
+          this.newProductForm.value.developers[i].developers
+        );
       }
       let newProduct = {
-        productId: "",
+        productId: '',
         productName: this.newProductForm.value.productName,
         productOwnerName: this.newProductForm.value.productOwner,
         developers: parseDevelopers,
         scrumMasterName: this.newProductForm.value.scrumMaster,
         startDate: this.newProductForm.value.startDate,
-        methodology: this.newProductForm.value.methodology
-      }
-      console.log(newProduct)
+        methodology: this.newProductForm.value.methodology,
+      };
+      console.log(newProduct);
       this.dialogRef.close(newProduct);
     }
   }
